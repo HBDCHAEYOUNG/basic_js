8ec99072e2a20166daa5e87fd7f0746614413256
HBDCHAEYOUNG <devyoon02@gmail.com>
2024-04-30 11:55:41 +0900

    js section2 퀴즈 풀이

--- javascript/review01.js
+++ javascript/review01.js
@@ -28,3 +28,50 @@ console.log("변경 후", x, y); //변경 후 Hello! 1
 // \\	백슬래시
 
 // ${}안에 상수나 변수, 표현식 등을 삽입 가능
+
+// !Section 2
+const a = 5;
+const b = "3";
+const c = a + b;
+console.log(c);
+//  ### 1. 어떤 값이 출력될까요? //!8틀림
+// 문자열+숫자 -> 이어붙인 문자열 반환 53
+
+const hour = 2;
+const minute = 30;
+//  const sentence =
+//  ### 2. '총 경과시간은 ~분입니다.'와 같은 문자열이 되도록
+//  템플릿 리터럴을 사용해서 작성해보세요.
+console.log(hour * 60 + minute);
+console.log(`총 경과시간은 ${hour * 60 + minute}분입니다.`);
+
+console.log(1 == "1", 1 === "1", 1 != "1", 1 !== "1");
+// ### 3.어떤 값이 출력될까요? true false false true
+
+let quiz_x = 3;
+console.log(quiz_x++, ++quiz_x, quiz_x--, --quiz_x);
+// ### 4.어떤 값이 출력될까요? 3,5,5,3
+
+let x5 = 5;
+let y5 = 10;
+let z = x5 > y5 && y5++;
+console.log(y5, z);
+//### 5. 어떤 값이 출력될까요? 10 false
+
+// ### 6.
+// ‘커피를 마시는 새’라는 책이 있습니다. 페이지수는 500, 품절은 되지 않은 상태고 장르는 판타지, SF, 스릴러입니다. 이 책의 정보를 **객체와 배열을 사용하여** 표현해보세요.
+let book = {
+  title: "커피를 마시는 새",
+  pages: 500,
+  stock: true,
+  genre: ["판타지", "SF", "스릴러"],
+};
+
+let quiz_a = 10;
+let quiz_b = quiz_a;
+quiz_a = 20;
+let quiz_c = { value: 10 };
+let d = quiz_c;
+quiz_c.value = 20;
+console.log(quiz_a, quiz_b, quiz_c.value, d.value);
+//  어떤 값이 출력될까요? 20, 10, 20, //!10틀림!참조타입임
