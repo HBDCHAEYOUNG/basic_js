a62eba36a0bf5824b262933d525b07f6fc3f7d3c
HBDCHAEYOUNG <devyoon02@gmail.com>
2024-05-30 00:21:34 +0900

    uppate: code js

--- javascript/codeup/6096.js
+++ javascript/codeup/6096.js
@@ -6,22 +6,77 @@
 // 출력
 // 십자 뒤집기 결과를 출력한다.
 
-const arrs = [];
-for (let i = 1; i <= 19; i++) {
-  const arr = [];
-  for (let j = 1; j <= 19; j++) {
-    arr.push(0);
-  }
-  arrs.push(arr);
-}
+// const arrs = [];
+// for (let i = 1; i <= 19; i++) {
+//   const arr = [];
+//   for (let j = 1; j <= 19; j++) {
+//     arr.push(0);
+//   }
+//   arrs.push(arr);
+// }
 
-function a(x, y) {
-  for (let i = 1; i <= 19; i++) {
-    arrs[i - 1][y - 1] = 1;
-    arrs[x - 1][i - 1] = 1;
-  }
-}
+// function a(x, y) {
+//   for (let i = 1; i <= 19; i++) {
+//     arrs[i - 1][y - 1] = 1;
+//     arrs[x - 1][i - 1] = 1;
+//   }
+// }
 
-a(10, 10), a(12, 12);
-console.log(arrs);
+// a(10, 10), a(12, 12);
+// console.log(arrs);
 //에휴..
+
+// !feedback
+const num = 19;
+const input = `0 0 0 0 0 0 0 0 0 1 0 1 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 1 0 1 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 1 0 1 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 1 0 1 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 1 0 1 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 1 0 1 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 1 0 1 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 1 0 1 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 1 0 1 0 0 0 0 0 0 0
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+0 0 0 0 0 0 0 0 0 1 0 1 0 0 0 0 0 0 0
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+0 0 0 0 0 0 0 0 0 1 0 1 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 1 0 1 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 1 0 1 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 1 0 1 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 1 0 1 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 1 0 1 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 1 0 1 0 0 0 0 0 0 0
+2
+10 10
+12 12`;
+
+const inputValue = input.split("\n");
+
+const graph = [];
+for (let i = 0; i < num; i++) {
+  const arrs = inputValue[i].split(" ").map(Number);
+  graph.push(arrs);
+}
+
+const n = inputValue[num];
+for (let i = 1; i <= n; i++) {
+  const [x, y] = inputValue[i + num].split(" ").map(Number);
+
+  // 2중 for
+  for (let j = 0; j < num; j++) {
+    // x좌표만 수정
+    if (graph[j][x - 1] === 1) graph[j][x - 1] = 0;
+    else graph[j][x - 1] = 1;
+
+    // y좌표만 수정
+    if (graph[y - 1][j] === 1) graph[y - 1][j] = 0;
+    else graph[y - 1][j] = 1;
+  }
+}
+
+// y = 10, x = 0 ~ 19 / 10 X
+// x = 10, y = 0 ~ 19
+for (let item of graph) {
+  console.log(item.join(" "));
+}
--- javascript/codeup/6097.js
+++ javascript/codeup/6097.js
@@ -20,36 +20,73 @@
 // 4 1 2 5`,
 // ];
 
-const arrs = [];
-for (let i = 1; i <= 5; i++) {
-  const arr = [];
-  for (let j = 1; j <= 5; j++) {
-    arr.push(0);
-  }
-  arrs.push(arr);
+// // 1. 채영이 스탈
+// const arrs = [];
+// for (let i = 1; i <= 5; i++) {
+//   const arr = [];
+//   for (let j = 1; j <= 5; j++) {
+//     arr.push(0);
+//   }
+//   arrs.push(arr);
+// }
+
+// const input = [
+//   [2, 0, [1, 1]],
+//   [3, 1, [2, 3]],
+//   [4, 1, [2, 5]],
+// ];
+// // 0 -> 몇번 반복
+// // 1 -> 0:가로 (x,y+0)
+// //      1:세로 (x+0,y)
+// // 2 -> 시작 좌표
+
+// for (let i = 0; i < 3; i++) {
+//   for (let j = 0; j < input[i][0]; j++) {
+//     if (input[i][1] === 0) {
+//       // 가로줄
+//       const [x, y] = input[i][2];
+//       arrs[x - 1][y - 1 + j] = 1;
+//     } else {
+//       // 세로줄
+//       const [x, y] = input[i][2];
+//       arrs[x - 1 + j][y - 1] = 1;
+//     }
+//   }
+// }
+// for (const arr of arrs) {
+//   console.log(arr.join(" "));
+// }
+
+// !feedback
+const input = `5 5
+3
+2 0 1 1
+3 1 2 3
+4 1 2 5`;
+
+const inputValue = input.split("\n");
+const [x, y] = inputValue[0].split(" ").map(Number);
+
+const graph = [];
+for (let i = 0; i < x; i++) graph.push(Array(y).fill(0));
+
+const n = Number(inputValue[1]);
+const testCase = [];
+for (let i = 2; i < n + 2; i++) {
+  testCase.push(inputValue[i].split(" ").map(Number));
 }
 
-const input = [
-  [2, 0, [1, 1]],
-  [3, 1, [2, 3]],
-  [4, 1, [2, 5]],
-];
-// 0 -> 몇번 반복
-// 1 -> 0:가로 (x,y+0)
-//      1:세로 (x+0,y)
-// 2 -> 시작 좌표
+for (let i = 0; i < n; i++) {
+  const [cnt, dir, dx, dy] = testCase[i];
+  const nx = dx - 1;
+  const ny = dy - 1;
 
-for (let i = 0; i < 3; i++) {
-  for (let j = 0; j < input[i][0]; j++) {
-    if (input[i][1] === 0) {
-      const [x, y] = input[i][2];
-      arrs[x - 1][y - 1 + j] = 1;
-    } else {
-      const [x, y] = input[i][2];
-      arrs[x - 1 + j][y - 1] = 1;
-    }
+  for (let j = 0; j < cnt; j++) {
+    if (dir === 0) graph[nx][ny + j] = 1;
+    else graph[nx + j][ny] = 1;
   }
 }
-for (const arr of arrs) {
-  console.log(arr.join(" "));
+
+for (const item of graph) {
+  console.log(item.join(" "));
 }
